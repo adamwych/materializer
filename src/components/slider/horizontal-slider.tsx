7bc28df2bfffe3190f8ba3ddbe41a197ff443bd4
Adam Wychowaniec <shoolygl@gmail.com>
2023-10-28 01:08:59 +0200

    fix: slider should update on color change

--- src/components/slider/horizontal-slider.tsx
+++ src/components/slider/horizontal-slider.tsx
@@ -4,7 +4,7 @@ import { SliderProps } from "./props";
 import defaultStyles from "./styles";
 
 export default function HorizontalSlider(props: SliderProps) {
-    const styles = defaultStyles(props.color ?? "gray");
+    const styles = () => defaultStyles(props.color ?? "gray");
     let trackElementRef: HTMLElement | undefined;
     const clampedValue = () => clamp(props.value, props.min, props.max);
 
@@ -17,22 +17,24 @@ export default function HorizontalSlider(props: SliderProps) {
     return (
         <div
             ref={(e) => (trackElementRef = e)}
-            class={styles.track}
+            class={styles().track}
             style={{
                 position: "relative",
                 width: "100%",
-                height: styles.thickness + "px",
+                height: styles().thickness + "px",
             }}
             onMouseDown={onMouseDown}
         >
             <div
-                class={styles.handle}
+                class={styles().handle}
                 style={{
                     position: "absolute",
-                    width: styles.handleSize[1] + "px",
-                    height: styles.handleSize[0] + "px",
+                    width: styles().handleSize[1] + "px",
+                    height: styles().handleSize[0] + "px",
                     left: `${(clampedValue() / props.max) * 100}%`,
-                    transform: `translate(-${styles.handleOffset[1]}px, -${styles.handleOffset[0]}px)`,
+                    transform: `translate(-${styles().handleOffset[1]}px, -${
+                        styles().handleOffset[0]
+                    }px)`,
                 }}
             ></div>
         </div>
--- src/components/slider/vertical-slider.tsx
+++ src/components/slider/vertical-slider.tsx
@@ -4,7 +4,7 @@ import { SliderProps } from "./props";
 import defaultStyles from "./styles";
 
 export default function VerticalSlider(props: SliderProps) {
-    const styles = defaultStyles(props.color ?? "gray");
+    const styles = () => defaultStyles(props.color ?? "gray");
     let trackElementRef: HTMLElement | undefined;
     const clampedValue = () => clamp(props.value, props.min, props.max);
 
@@ -17,22 +17,24 @@ export default function VerticalSlider(props: SliderProps) {
     return (
         <div
             ref={(e) => (trackElementRef = e)}
-            class={styles.track}
+            class={styles().track}
             style={{
                 position: "relative",
-                width: styles.thickness + "px",
+                width: styles().thickness + "px",
                 height: "100%",
             }}
             onMouseDown={onMouseDown}
         >
             <div
-                class={styles.handle}
+                class={styles().handle}
                 style={{
                     position: "absolute",
-                    width: styles.handleSize[0] + "px",
-                    height: styles.handleSize[1] + "px",
+                    width: styles().handleSize[0] + "px",
+                    height: styles().handleSize[1] + "px",
                     bottom: `${(clampedValue() / props.max) * 100}%`,
-                    transform: `translate(-${styles.handleOffset[0]}px, ${styles.handleOffset[1]}px)`,
+                    transform: `translate(-${styles().handleOffset[0]}px, ${
+                        styles().handleOffset[1]
+                    }px)`,
                 }}
             ></div>
         </div>
