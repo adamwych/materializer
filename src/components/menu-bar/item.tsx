0a8a66d4d11a94a44bd4e63c9fc718d55d628efc
Adam Wychowaniec <shoolygl@gmail.com>
2023-11-02 18:24:00 +0100

    feat: icon in a MenuBarItem

--- src/components/menu-bar/item.tsx
+++ src/components/menu-bar/item.tsx
@@ -1,14 +1,17 @@
+import { Component } from "solid-js";
 import { useMenuBarContext } from "./context.ts";
 
 interface Props {
     label: string;
     shortcut?: string;
+    icon?: Component<{ size: number }>;
 
     onClick?(): void;
 }
 
 export default function MenuBarItem(props: Props) {
     const context = useMenuBarContext();
+    const IconComponent = props.icon;
 
     function onClick() {
         props.onClick?.();
@@ -18,7 +21,10 @@ export default function MenuBarItem(props: Props) {
     return (
         <div class="px-3 py-2 text-sm hover:bg-gray-200 active:bg-gray-300" onClick={onClick}>
             <div class="flex items-center justify-between">
-                <span>{props.label}</span>
+                <span class="flex items-center gap-2">
+                    {IconComponent && <IconComponent size={16} />}
+                    {props.label}
+                </span>
                 <span class="text-xs text-gray-700">{props.shortcut}</span>
             </div>
         </div>
