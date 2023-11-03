ca30f0969013f570bc4114a7e62ec789cdba4795
Adam Wychowaniec <shoolygl@gmail.com>
2023-11-03 04:49:05 +0100

    fix: order of operations in blend node

--- glsl/blend.glsl
+++ glsl/blend.glsl
@@ -11,16 +11,16 @@ uniform sampler2D i_background;
 out vec4 out_color;
 
 void main(void) {
-    out_color = texture(i_foreground, a_texCoord);
+    out_color = texture(i_background, a_texCoord);
 
     if (p_mode == 0) {
-        out_color += texture(i_background, a_texCoord) * p_intensity;
+        out_color += texture(i_foreground, a_texCoord) * p_intensity;
     } else if (p_mode == 1) {
-        out_color -= texture(i_background, a_texCoord) * p_intensity;
+        out_color -= texture(i_foreground, a_texCoord) * p_intensity;
     } else if (p_mode == 2) {
-        out_color *= texture(i_background, a_texCoord) * p_intensity;
+        out_color *= texture(i_foreground, a_texCoord) * p_intensity;
     } else if (p_mode == 3) {
-        out_color /= texture(i_background, a_texCoord) * p_intensity;
+        out_color /= texture(i_foreground, a_texCoord) * p_intensity;
     }
     
     out_color.a = 1.0;
