7e812886801df42d4755e50fa24bcc3fff8b41fb
Adam Wychowaniec <shoolygl@gmail.com>
2023-11-17 05:55:56 +0100

    Prevent division by zero in the Blend node

--- resources/nodes/base/blend.glsl
+++ resources/nodes/base/blend.glsl
@@ -10,18 +10,28 @@ uniform sampler2D i_background;
 
 out vec4 out_color;
 
-void main(void) {
-    vec3 color = texture(i_background, a_texCoord).rgb;
+#define BLEND_MODE_ADD 0
+#define BLEND_MODE_SUBTRACT 1
+#define BLEND_MODE_MULTIPLY 2
+#define BLEND_MODE_DIVIDE 3
 
-    if(p_mode == 0) {
-        color += texture(i_foreground, a_texCoord).rgb * p_intensity;
-    } else if(p_mode == 1) {
-        color -= texture(i_foreground, a_texCoord).rgb * p_intensity;
-    } else if(p_mode == 2) {
-        color *= texture(i_foreground, a_texCoord).rgb * p_intensity;
-    } else if(p_mode == 3) {
-        color /= texture(i_foreground, a_texCoord).rgb * p_intensity;
+void main(void) {
+    vec3 backgroundColor = texture(i_background, a_texCoord).rgb;
+    vec3 foregroundColor = texture(i_foreground, a_texCoord).rgb * p_intensity;
+
+    if(p_mode == BLEND_MODE_ADD) {
+        backgroundColor += foregroundColor;
+    } else if(p_mode == BLEND_MODE_SUBTRACT) {
+        backgroundColor -= foregroundColor;
+    } else if(p_mode == BLEND_MODE_MULTIPLY) {
+        backgroundColor *= foregroundColor;
+    } else if(p_mode == BLEND_MODE_DIVIDE) {
+        // Clamp all values to prevent division by zero.
+        foregroundColor.r = clamp(foregroundColor.r, 1.0f / 255.0f, 1.0f);
+        foregroundColor.g = clamp(foregroundColor.g, 1.0f / 255.0f, 1.0f);
+        foregroundColor.b = clamp(foregroundColor.b, 1.0f / 255.0f, 1.0f);
+        backgroundColor /= foregroundColor;
     }
 
-    out_color = vec4(color, 1);
+    out_color = vec4(backgroundColor, 1);
 }
